894781e7a90144d71f204f4f462300b02566f5b7
Marcelino Yax <marce7j@gmail.com>
2025-05-04 23:27:31 -0400

    remove the safe max throtle in the RC channels

--- src/RC_Channels/RC_Channels.cpp
+++ src/RC_Channels/RC_Channels.cpp
@@ -25,7 +25,6 @@ float RC_Channels::computeDesiredRate(uint16_t inputInPWM)
 float RC_Channels::computeDesiredAngle(uint16_t inputInPWM)
 {
     return DESIRED_ANGLE_FACTOR * (inputInPWM - RC_MID_CHANNEL_VALUE);
-
 }
 
 void RC_Channels::init()
@@ -46,10 +45,11 @@ void RC_Channels::read()
 
         _pwm_channels.roll = constrain(rollValue, RC_MIN_CHANNEL_VALUE, RC_MAX_CHANNEL_VALUE);
         _pwm_channels.pitch = constrain(pitchValue, RC_MIN_CHANNEL_VALUE, RC_MAX_CHANNEL_VALUE);
+        _pwm_channels.throttle = constrain(throttleValue, RC_MIN_CHANNEL_VALUE, RC_MAX_CHANNEL_VALUE);
         _pwm_channels.yaw = constrain(yawValue, RC_MIN_CHANNEL_VALUE, RC_MAX_CHANNEL_VALUE);
 
-        uint16_t constrainThrottle = constrain(throttleValue, RC_MIN_CHANNEL_VALUE, RC_MAX_CHANNEL_VALUE);
-        _pwm_channels.throttle = constrainThrottle > RC_MAX_THROTTLE ? RC_MAX_THROTTLE : constrainThrottle;
+        // uint16_t constrainThrottle = constrain(throttleValue, RC_MIN_CHANNEL_VALUE, RC_MAX_CHANNEL_VALUE);
+        // _pwm_channels.throttle = constrainThrottle > RC_MAX_THROTTLE ? RC_MAX_THROTTLE : constrainThrottle;
 
         _desired_rates.roll = computeDesiredRate(_pwm_channels.roll);
         _desired_rates.pitch = computeDesiredRate(_pwm_channels.pitch);
