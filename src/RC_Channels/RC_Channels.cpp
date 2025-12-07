c4d179661870971c8b47b5c9a2d742ba0bb13d93
Marcelino Yax <marce7j@gmail.com>
2025-12-07 00:26:51 -0500

    make the channels as an array of 8 channels for easy iteration

--- src/RC_Channels/RC_Channels.cpp
+++ src/RC_Channels/RC_Channels.cpp
@@ -41,9 +41,9 @@ float RC_Channels::compute_desired_velocity(uint16_t input_in_pwm)
     return DESIRED_VELOCITY_FACTOR * (input_in_pwm - RC_MID_CHANNEL_VALUE);
 }
 
-bool RC_Channels::is_motor_emergency(uint16_t aux_1, uint16_t aux_2)
+bool RC_Channels::check_motor_emergency(uint16_t pwm)
 {
-    return (_pwm_channels.aux_4 >= RC_MAX_CHANNEL_VALUE - 1) || ((aux_1 >= RC_AUX_CHANNEL_HIGH_VALUE) && (aux_2 >= RC_AUX_CHANNEL_HIGH_VALUE));
+    return pwm >= (RC_MAX_CHANNEL_VALUE - RC_DEAD_ZONE);
 }
 
 void RC_Channels::init()
@@ -59,23 +59,12 @@ void RC_Channels::read()
     {
         bfs::SbusData data = _sbus_rx.data();
 
-        uint16_t roll = map_sbus_to_pwm(data.ch[RC_CHANNEL_IDX_ROLL]);
-        uint16_t pitch = map_sbus_to_pwm(data.ch[RC_CHANNEL_IDX_PITCH]);
-        uint16_t throttle = map_sbus_to_pwm(data.ch[RC_CHANNEL_IDX_THROTTLE]);
-        uint16_t yaw = map_sbus_to_pwm(data.ch[RC_CHANNEL_IDX_YAW]);
-        uint16_t aux_1 = map_sbus_to_pwm(data.ch[RC_CHANNEL_IDX_AUX_1]);
-        uint16_t aux_2 = map_sbus_to_pwm(data.ch[RC_CHANNEL_IDX_AUX_2]);
-        uint16_t aux_3 = map_sbus_to_pwm(data.ch[RC_CHANNEL_IDX_AUX_3]);
-        uint16_t aux_4 = map_sbus_to_pwm(data.ch[RC_CHANNEL_IDX_AUX_4]);
-
-        _pwm_channels.roll = constrain(roll, RC_MIN_CHANNEL_VALUE, RC_MAX_CHANNEL_VALUE);
-        _pwm_channels.pitch = constrain(pitch, RC_MIN_CHANNEL_VALUE, RC_MAX_CHANNEL_VALUE);
-        _pwm_channels.throttle = constrain(throttle, RC_MIN_CHANNEL_VALUE, RC_MAX_CHANNEL_VALUE);
-        _pwm_channels.yaw = constrain(yaw, RC_MIN_CHANNEL_VALUE, RC_MAX_CHANNEL_VALUE);
-        _pwm_channels.aux_1 = constrain(aux_1, RC_MIN_CHANNEL_VALUE, RC_MAX_CHANNEL_VALUE);
-        _pwm_channels.aux_2 = constrain(aux_2, RC_MIN_CHANNEL_VALUE, RC_MAX_CHANNEL_VALUE);
-        _pwm_channels.aux_3 = constrain(aux_3, RC_MIN_CHANNEL_VALUE, RC_MAX_CHANNEL_VALUE);
-        _pwm_channels.aux_4 = constrain(aux_4, RC_MIN_CHANNEL_VALUE, RC_MAX_CHANNEL_VALUE);
+        for (int index = 0; index < NUM_CHANNELS; index++)
+        {
+            uint16_t pwm = map_sbus_to_pwm(data.ch[index]);
+            pwm = constrain(pwm, RC_MIN_CHANNEL_VALUE, RC_MAX_CHANNEL_VALUE);
+            channels[index].pwm = pwm;
+        }
 
         _last_radio_reading_ms = now_ms;
         _has_received_radio_reading = true;
--- src/RC_Channels/RC_Channels.h
+++ src/RC_Channels/RC_Channels.h
@@ -3,21 +3,22 @@
 
 #include <sbus.h>
 
-#define RC_CHANNEL_IDX_ROLL 0
-#define RC_CHANNEL_IDX_PITCH 1
-#define RC_CHANNEL_IDX_THROTTLE 2
-#define RC_CHANNEL_IDX_YAW 3
-#define RC_CHANNEL_IDX_AUX_1 4
-#define RC_CHANNEL_IDX_AUX_2 5
-#define RC_CHANNEL_IDX_AUX_3 6
-#define RC_CHANNEL_IDX_AUX_4 7
+#define NUM_CHANNELS 8
+#define RC_CHANNEL_1 0
+#define RC_CHANNEL_2 1
+#define RC_CHANNEL_3 2
+#define RC_CHANNEL_4 3
+#define RC_CHANNEL_5 4
+#define RC_CHANNEL_6 5
+#define RC_CHANNEL_7 6
+#define RC_CHANNEL_8 7
 
 // The range of a channel's possible values (microseconds)
 #define RC_CHANNEL_DEFAULT_VAL 1000
 #define RC_MIN_CHANNEL_VALUE 1000
 #define RC_MAX_CHANNEL_VALUE 2000
 #define RC_MID_CHANNEL_VALUE 1500
-#define RC_AUX_CHANNEL_HIGH_VALUE 1900
+#define RC_DEAD_ZONE 5
 
 #define MIN_PWM 1000
 #define MAX_PWM 2000
@@ -37,22 +38,18 @@ public:
     }
     void init();
     void read();
-    uint16_t get_roll_in_pwm() { return _pwm_channels.roll; }
-    uint16_t get_pitch_in_pwm() { return _pwm_channels.pitch; }
-    uint16_t get_throttle_in_pwm() { return _pwm_channels.throttle; }
-    uint16_t get_yaw_in_pwm() { return _pwm_channels.yaw; }
-    uint16_t get_aux_1_in_pwm() { return _pwm_channels.aux_1; }
-    uint16_t get_aux_2_in_pwm() { return _pwm_channels.aux_2; }
-    uint16_t get_aux_3_in_pwm() { return _pwm_channels.aux_3; }
-    uint16_t get_aux_4_in_pwm() { return _pwm_channels.aux_4; }
+    uint16_t get_roll_in_pwm() { return channels[RC_CHANNEL_1].pwm; }
+    uint16_t get_pitch_in_pwm() { return channels[RC_CHANNEL_2].pwm; }
+    uint16_t get_throttle_in_pwm() { return channels[RC_CHANNEL_3].pwm; }
+    uint16_t get_yaw_in_pwm() { return channels[RC_CHANNEL_4].pwm; }
     float get_mid_throttle() { return RC_MID_CHANNEL_VALUE; }
-    float get_desired_roll_rate() { return compute_expo_desired_rate(_pwm_channels.roll); }
-    float get_desired_pitch_rate() { return compute_expo_desired_rate(_pwm_channels.pitch); }
-    float get_desired_yaw_rate() { return -1.0 * compute_expo_desired_rate(_pwm_channels.yaw); }
-    float get_desired_roll_angle() { return compute_desired_angle(_pwm_channels.roll); }
-    float get_desired_pitch_angle() { return compute_desired_angle(_pwm_channels.pitch); }
-    float get_desired_vertical_velocity() { return compute_desired_velocity(_pwm_channels.throttle); }
-    bool is_motor_emergency() { return is_motor_emergency(_pwm_channels.aux_1, _pwm_channels.aux_3); }
+    float get_desired_roll_rate() { return compute_expo_desired_rate(get_roll_in_pwm()); }
+    float get_desired_pitch_rate() { return compute_expo_desired_rate(get_pitch_in_pwm()); }
+    float get_desired_yaw_rate() { return -1.0 * compute_expo_desired_rate(get_yaw_in_pwm()); }
+    float get_desired_roll_angle() { return compute_desired_angle(get_roll_in_pwm()); }
+    float get_desired_pitch_angle() { return compute_desired_angle(get_pitch_in_pwm()); }
+    float get_desired_vertical_velocity() { return compute_desired_velocity(get_throttle_in_pwm()); }
+    bool is_motor_emergency() { return check_motor_emergency(channels[RC_CHANNEL_8].pwm); }
     bool is_radio_failsafe() { return _radio_failsafe; }
     bool has_received_radio_reading()
     {
@@ -60,26 +57,19 @@ public:
     }
 
 private:
-    struct pwm_channels_t
+    struct channel
     {
-        uint16_t roll;
-        uint16_t pitch;
-        uint16_t throttle;
-        uint16_t yaw;
-        uint16_t aux_1;
-        uint16_t aux_2;
-        uint16_t aux_3;
-        uint16_t aux_4;
+        uint16_t pwm;
     };
 
-    pwm_channels_t _pwm_channels = {RC_CHANNEL_DEFAULT_VAL,
-                                    RC_CHANNEL_DEFAULT_VAL,
-                                    RC_CHANNEL_DEFAULT_VAL,
-                                    RC_CHANNEL_DEFAULT_VAL,
-                                    RC_CHANNEL_DEFAULT_VAL,
-                                    RC_CHANNEL_DEFAULT_VAL,
-                                    RC_CHANNEL_DEFAULT_VAL,
-                                    RC_CHANNEL_DEFAULT_VAL};
+    channel channels[NUM_CHANNELS] = {{RC_CHANNEL_DEFAULT_VAL},
+                                      {RC_CHANNEL_DEFAULT_VAL},
+                                      {RC_CHANNEL_DEFAULT_VAL},
+                                      {RC_CHANNEL_DEFAULT_VAL},
+                                      {RC_CHANNEL_DEFAULT_VAL},
+                                      {RC_CHANNEL_DEFAULT_VAL},
+                                      {RC_CHANNEL_DEFAULT_VAL},
+                                      {RC_CHANNEL_DEFAULT_VAL}};
     bfs::SbusRx _sbus_rx;
     bool _radio_failsafe = false;
     bool _has_received_radio_reading = false;
@@ -89,5 +79,5 @@ private:
     float compute_expo_desired_rate(uint16_t input_in_pwm);
     float compute_desired_angle(uint16_t input_in_pwm);
     float compute_desired_velocity(uint16_t input_in_pwm);
-    bool is_motor_emergency(uint16_t aux_1, uint16_t aux_2);
+    bool check_motor_emergency(uint16_t pwm);
 };
